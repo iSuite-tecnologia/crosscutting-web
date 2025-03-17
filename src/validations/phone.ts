e0c453ab62adeb10bfb53c7f201bfaa6bfdff919
Anderson Saraiva <a.saraiva@useflui.com.br>
2025-03-17 13:19:03 -0300

    feat: adicionar nova validacao para telefone

---
+++ src/validations/phone.ts
@@ -0,0 +1,19 @@
+/**
+ * Faz a validação de um Telefone
+ * @param value - Valor para ser verificado
+ * @returns Uma mensagem em caso de erro ou true como valor verificado
+ * @example
+ *
+ * const value = validatePhone('11975110119');
+ * console.log(value); // true
+ */
+
+export const validatePhone = (
+  value: string | null | undefined,
+  message: string
+): string | boolean => {
+  if (!value) return true;
+
+  const REGEX = /\(\d{2}\)\s*\d{4,5}-\d{4}|\d{10,11}/;
+  return REGEX.test(value) || message;
+};
